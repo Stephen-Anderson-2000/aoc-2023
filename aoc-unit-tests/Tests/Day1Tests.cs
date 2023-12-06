31c5715e21b3fb745e0a65305738b751c323d5e6
Stephen Anderson <steppyandrews@gmail.com>
2023-12-06 14:00:37 -0800

    Day 1 part 2 (which I missed in my sleep deprived state last night)

--- aoc-unit-tests/Tests/Day1Tests.cs
+++ aoc-unit-tests/Tests/Day1Tests.cs
@@ -3,33 +3,42 @@ using FluentAssertions;
 namespace aoc_unit_tests.Tests;
 public class Day1Tests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("1abc2", 12)]
+    [InlineData("pqr3stu8vwx", 38)]
+    [InlineData("a1b2c3d4e5f", 15)]
+    [InlineData("treb7uchet", 77)]
+    [InlineData("two1nine", 29)]
+    [InlineData("eightwothree", 83)]
+    [InlineData("abcone2threexyz", 13)]
+    [InlineData("xtwone3four", 24)]
+    [InlineData("4nineeightseven2", 42)]
+    [InlineData("zoneight234", 14)]
+    [InlineData("7pqrstsixteen", 76)]
+    [InlineData("one", 11)]
+    [InlineData("twone", 21)]
+    public void Test1(string input, int expected)
     {
-        Day1.GetCalibrationFromString("1abc2").Should().Be(12);
+        Day1.GetCalibrationFromString(input).Should().Be(expected);
     }
 
     [Fact]
     public void Test2()
     {
-        Day1.GetCalibrationFromString("pqr3stu8vwx").Should().Be(38);
+        Day1.SumCalibrationValues(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]).Should().Be(142);
     }
 
     [Fact]
     public void Test3()
     {
-        Day1.GetCalibrationFromString("a1b2c3d4e5f").Should().Be(15);
-    }
-
-    [Fact]
-    public void Test4()
-    {
-        Day1.GetCalibrationFromString("treb7uchet").Should().Be(77);
-    }
-
-    [Fact]
-    public void Test5()
-    {
-        Day1.SumCalibrationValues([ "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" ]).Should().Be(142);
+        Day1.SumCalibrationValues([
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"])
+        .Should().Be(281);
     }
 }
--- src/Days/Day1.cs
+++ src/Days/Day1.cs
@@ -3,10 +3,37 @@
 namespace aoc_2023.Days;
 public class Day1
 {
+    private static readonly Dictionary<string, string> numberDict = new() {
+        { "one", "1" },
+        { "two", "2" },
+        { "three", "3" },
+        { "four", "4" },
+        { "five", "5" },
+        { "six", "6" },
+        { "seven", "7" },
+        { "eight", "8" },
+        { "nine", "9" }
+    };
+
+    /// <summary>
+    /// This is a stupid solution and I fucking hate RegEx
+    /// </summary>
     public static int GetCalibrationFromString(string calString)
     {
-        var matches = Regex.Matches(calString, "[0-9]");
-        return matches.Count > 0 ? int.Parse(matches.First().Value + matches.Last().Value) : 0;
+        var first = Regex.Matches(calString, "[0-9]|one|two|three|four|five|six|seven|eight|nine");
+        var charArray = calString.ToCharArray();
+        Array.Reverse(charArray);
+
+        var last = Regex.Matches(new string(charArray), "enin|thgie|neves|xis|evif|ruof|eerht|owt|eno|[0-9]");
+        charArray = last[0].Value.ToCharArray();
+        Array.Reverse(charArray);
+
+        return int.Parse(ParseNumberWord(first[0].Value) + ParseNumberWord(new string(charArray)));
+    }
+
+    private static string ParseNumberWord(string input)
+    {
+        return !int.TryParse(input, out var _) ? numberDict[input] : input;
     }
 
     public static int SumCalibrationValues(string[] calStrings)
